6c4a95abc4d90dc7398ca28bdd22195b2b10c2c8
Victor <victorobioratech@gmail.com>
2023-11-01 14:38:21 +0100

    form validation being built for register component

--- src/components/general/RegisterComponent.tsx
+++ src/components/general/RegisterComponent.tsx
@@ -1,42 +1,139 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import classes from "./RegisterComponent.module.css";
 import Link from "next/link";
 
-const RegisterComponent: React.FC = (props) => {
+const RegisterComponent: React.FC<{
+  removeRegComponent: () => void;
+}> = (props) => {
   const [isReg, setIsReg] = useState<boolean>(true);
+  const [inputNumberClass, setInputNumberClass] = useState<string>(
+    classes.input_text
+  );
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+  const [registerDetails, setRegisterDetails] = useState<{
+    regPhoneNumber: string;
+    regPassword: string;
+  }>({
+    regPhoneNumber: "",
+    regPassword: "",
+  });
 
   const regOrLoginClass = isReg
     ? `${classes.register_on}`
     : `${classes.login_on}`;
 
+  let buttonClassName = buttonDisabled
+    ? `${classes.formButton} ${classes.disabled}`
+    : `${classes.formButton}`;
+
+  const removeComponentHandler = (): void => {
+    props.removeRegComponent();
+  };
+
+  const updateModeToReg = (): void => {
+    setIsReg(true);
+  };
+  const updateModeToLogin = (): void => {
+    setIsReg(false);
+  };
+
+  const updatePasswordHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRegisterDetails((prevState) => {
+      return {
+        ...prevState,
+        regPasssword: event.target.value,
+      };
+    });
+  };
+  const updateRegNumberHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRegisterDetails((prevState) => {
+      return {
+        ...prevState,
+        regPhoneNumber: event.target.value,
+      };
+    });
+  };
+
+  console.log(registerDetails)
+
   return (
     <section className={classes.container}>
+      <main className={classes.backdrop}></main>
       <main className={classes.main_form}>
+        <div className={classes.exitButton} onClick={removeComponentHandler}>
+          X
+        </div>
         <div className={`${classes.reg_form_header} ${regOrLoginClass}`}>
-          <div>Register</div>
-          <div>Login</div>
+          <section>
+            <div className={classes.regLog}>
+              <div onClick={updateModeToReg}>Register</div>
+              <div onClick={updateModeToLogin}>Login</div>
+            </div>
+            <span></span>
+          </section>
         </div>
         <form className={classes.reg_form}>
-          <div>
-            <input type="text" />
-            <div className={classes.input_password}>
-              <input type="password" />
+          <div className={classes.inputData}>
+            <div className={inputNumberClass}>
+              <span>+234</span>
+              <input
+                type="text"
+                placeholder="Mobile Number"
+                onChange={updateRegNumberHandler}
+                onMouseDown={(): void => {
+                  setInputNumberClass(
+                    `${classes.input_text} ${classes.addNumberEffect}`
+                  );
+                }}
+                onBlur={(): void => {
+                  setInputNumberClass(`${classes.input_text}`);
+                }}
+              />
+            </div>
+            <div
+              className={classes.input_password}
+              onChange={updatePasswordHandler}
+            >
+              <input type="password" placeholder="Set Password" />
             </div>
           </div>
           {!isReg && (
             <div className={classes.login_preferences}>
-              <div>
-                <input type="checkbox" name="Remember Me" />
-                <input type="checkbox" name="Keep me signed In" />
-              </div>
-              <Link href="">Forgot Password</Link>
+              <ul>
+                <li>
+                  <input type="checkbox" name="Remember Me" />
+                  <label htmlFor="Remember Me">Remember me</label>
+                </li>
+                <li>
+                  <input type="checkbox" name="Keep me signed In" />
+                  <label htmlFor="Keep me signed In">Keep me signed In</label>
+                </li>
+              </ul>
+              <Link href="">Forgot Password ? </Link>
             </div>
           )}
 
-          <button>{isReg ? "Create New Account" : "Login"}</button>
+          <button disabled={buttonDisabled} className={buttonClassName}>
+            {isReg ? "Create New Account" : "Login"}
+          </button>
+          {isReg && (
+            <p className={classes.compliance_text}>
+              By creating an account, you agree to our Terms & Conditions and
+              confirm that you are at least <br /> 18 years old or over and all
+              information given is true.
+            </p>
+          )}
+          <section className={classes.ball}>
+            <img src="https://s.sporty.net/global/main/modules/main/desktop/common/components/login/image/popupBall2.0aaeb1c629.png" />
+          </section>
         </form>
-        <div className={classes.ball}></div>
       </main>
     </section>
   );
 };
+
+export default RegisterComponent;
--- src/components/general/SignInForm.tsx
+++ src/components/general/SignInForm.tsx
@@ -1,28 +1,42 @@
+import { useState } from "react";
 import classes from "./SignInForm.module.css";
+import RegisterComponent from "./RegisterComponent";
 
 const SignInForm: React.FC = () => {
+  const [showRegComponent, setShowRegComponent] = useState<boolean>(false);
 
   const checkLogin = () => {
-    //..check if login details are correct 
-  }
+    //..check if login details are correct
+  };
 
-  const registerButtonHandler = (event:React.FormEvent) => {
-    event.preventDefault()
+  const registerButtonHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    setShowRegComponent(true)
+  };
+
+  const removeRegHandler = () => {
+    setShowRegComponent(false)
   }
   return (
-    <form className={classes.form_container}>
-      <div className={classes.form_getInput}>
-        <span>+234</span>
-        <input type="text" placeholder="Mobile Number" />
-      </div>
-      <div className={classes.form_getInput}>
-        <input type="password" placeholder="Password" />
-        <span className={classes.login_button} onClick={checkLogin}>Login</span>
-      </div>
-      <div className={classes.form_getInput}>
-        <button onClick={registerButtonHandler}> Register </button>
-      </div>
-    </form>
+    <>
+      {" "}
+      <form className={classes.form_container}>
+        <div className={classes.form_getInput}>
+          <span>+234</span>
+          <input type="text" placeholder="Mobile Number" />
+        </div>
+        <div className={classes.form_getInput}>
+          <input type="password" placeholder="Password" />
+          <span className={classes.login_button} onClick={checkLogin}>
+            Login
+          </span>
+        </div>
+        <div className={classes.form_getInput}>
+          <button onClick={registerButtonHandler}> Register </button>
+        </div>
+      </form>
+      {showRegComponent && <RegisterComponent removeRegComponent= {removeRegHandler}/>}
+    </>
   );
 };
 
