4724bdde645f81b9064dd6e2cb281b213f7aff03
Victor <victorobioratech@gmail.com>
2023-11-20 21:03:48 +0100

    nofication on account creation success being built

--- src/components/general/SignInForm.tsx
+++ src/components/general/SignInForm.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import classes from "./SignInForm.module.css";
 import RegisterComponent from "./RegisterComponent";
-
+import { useAppSelector } from "@/store/hooks";
+import { useAppDispatch } from "@/store/hooks";
 
 const SignInForm: React.FC = () => {
+  const isLoggedIn = useAppSelector(
+    (state) => state.logInStatus.areWeLoggedIn
+  );
+  const dispatch = useAppDispatch();
   const [showRegComponent, setShowRegComponent] = useState<boolean>(false);
 
   const checkLogin = () => {
@@ -12,30 +17,37 @@ const SignInForm: React.FC = () => {
 
   const registerButtonHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    setShowRegComponent(true)
+    setShowRegComponent(true);
   };
 
   const removeRegHandler = () => {
-    setShowRegComponent(false)
-  }
+    setShowRegComponent(false);
+  };
   return (
     <>
-      {" "}
-      <form className={classes.form_container}>
-        <div className={classes.form_getInput}>
-          <input type="text" placeholder="Email" />
-        </div>
-        <div className={classes.form_getInput}>
-          <input type="password" placeholder="Password" />
-          <span className={classes.login_button} onClick={checkLogin}>
-            Login
-          </span>
-        </div>
-        <div className={classes.form_getInput}>
-          <button onClick={registerButtonHandler}> Register </button>
-        </div>
-      </form>
-      {showRegComponent && <RegisterComponent removeRegComponent= {removeRegHandler}/>}
+      {!isLoggedIn && (
+        <form className={classes.form_container}>
+          <div className={classes.form_getInput}>
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className={classes.form_getInput}>
+            <input type="password" placeholder="Password" />
+            <span className={classes.login_button} onClick={checkLogin}>
+              Login
+            </span>
+          </div>
+          <div className={classes.form_getInput}>
+            <button onClick={registerButtonHandler}> Register </button>
+          </div>
+        </form>
+      )}
+{isLoggedIn &&       <div className={classes.logout_button}>
+        <button onClick={registerButtonHandler}> Log Out </button>
+      </div>}
+
+      {showRegComponent || isLoggedIn && (
+        <RegisterComponent removeRegComponent={removeRegHandler} />
+      )}
     </>
   );
 };
